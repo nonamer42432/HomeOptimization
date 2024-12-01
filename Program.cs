3667ee8519a283dc2fd4a04ced72b6b81038e736
Gamerofgtaonline <61492510+nonamer42432@users.noreply.github.com>
2024-12-01 10:53:26 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Это пример текста. Текст должен быть проанализирован. Это важно.";
+        Dictionary<string, int> wordCounts = WordFrequency(text);
+
+        foreach (var item in wordCounts)
+        {
+            // Используем интерполяцию для читаемости
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+    }
+
+    static Dictionary<string, int> WordFrequency(string text)
+    {
+        // Используем Linq для компановки и эффективности кода
+        return text.Split(new char[] { ' ', ',', '!' }, StringSplitOptions.RemoveEmptyEntries).GroupBy(word => word.ToLower()).ToDictionary(group => group.Key, group => group.Count());
+        // Разбиваем строку на слова, удаляя пустые элементы
+        // Групперуем слова по регистрации ключа
+        // Создаем словарь частоты
+    }
+}
